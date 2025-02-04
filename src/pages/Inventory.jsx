213012e11e2272cbd8696ec8d31fc6aa15803b64
FrederickLamptey <102755255+FrederickLamptey@users.noreply.github.com>
2025-02-04 17:55:56 -0500

    set up project

---
+++ src/pages/Inventory.jsx
@@ -0,0 +1,13 @@
+import Heading from '../ui/Heading';
+import Row from '../ui/Row';
+
+function Inventory() {
+  return (
+    <Row type="horizontal">
+      <Heading as="h1">All Items</Heading>
+      <p>TEST</p>
+    </Row>
+  );
+}
+
+export default Inventory;
---
+++ src/pages/Purchases.jsx
@@ -0,0 +1,13 @@
+import Heading from '../ui/Heading';
+import Row from '../ui/Row';
+
+function Purchases() {
+  return (
+    <Row type="horizontal">
+      <Heading as="h1">All purchases</Heading>
+      <p>TEST</p>
+    </Row>
+  );
+}
+
+export default Purchases;
