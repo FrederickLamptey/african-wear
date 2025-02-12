4d0b7c8d0f5a9f99bfaa08f41c3a5b4329515f05
FrederickLamptey <102755255+FrederickLamptey@users.noreply.github.com>
2025-02-11 19:45:54 -0500

    create supabase client

--- src/pages/Inventory.jsx
+++ src/pages/Inventory.jsx
@@ -1,11 +1,18 @@
+import { useEffect } from 'react';
 import Heading from '../ui/Heading';
 import Row from '../ui/Row';
+import { getInventory } from '../services/apiInventory';
 
 function Inventory() {
+  useEffect(function () {
+    getInventory().then((data) => console.log(data));
+  }, []);
+
   return (
     <Row type="horizontal">
       <Heading as="h1">All Items</Heading>
       <p>TEST</p>
+      <img src="https://zwtxzfvoordeefpkxigr.supabase.co/storage/v1/object/public/inventory-images//dinero-kaftan-1.jpg" />
     </Row>
   );
 }
