52c8a3d8ace227aa2a282ecdff9faa5ad4106387
FrederickLamptey <102755255+FrederickLamptey@users.noreply.github.com>
2025-02-21 17:39:23 -0500

    add comments

--- src/features/Purchases/useFetchPurchases.js
+++ src/features/Purchases/useFetchPurchases.js
@@ -3,18 +3,24 @@ import { getPurchases } from '../../services/apiPurchases';
 import { useSearchParams } from 'react-router-dom';
 
 export function useFetchPurchases() {
-  const [searchParams] = useSearchParams()
+  const [searchParams] = useSearchParams();
 
-  // get state value of filter from url 
-  const filterValue = searchParams.get("status");
-  const filter = !filterValue || filterValue === "all" ? null : { field: "status", value: filterValue, method: "eq" };
+  // get state value of filter from url
+  const filterValue = searchParams.get('status');
+
+//conditional filter value setting 
+  const filter =
+    !filterValue || filterValue === 'all'
+      ? null
+      : { field: 'status', value: filterValue, method: 'eq' };
+  
   const {
     isLoading,
     data: purchases = [],
     error,
   } = useQuery({
     queryKey: ['purchases', filter],
-    queryFn: () => getPurchases({filter}),
+    queryFn: () => getPurchases({ filter }),
   });
 
   return { isLoading, error, purchases };
