1b4fdabee073ed5e0b5e3e4be27510685b6a0ba8
FrederickLamptey <102755255+FrederickLamptey@users.noreply.github.com>
2025-02-21 16:47:17 -0500

    change column name in purchases

--- src/features/Purchases/PurchaseTable.jsx
+++ src/features/Purchases/PurchaseTable.jsx
@@ -16,7 +16,7 @@ function PurchaseTable() {
       <Table columns="0.6fr 2fr 2.4fr 1.4fr 1fr 3.2rem">
         <Table.Header>
           <div>Item</div>
-          <div>Guest</div>
+          <div>Customer</div>
           <div>Status</div>
           <div>Amount</div>
           <div>date</div>
